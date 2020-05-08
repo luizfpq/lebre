29b568783773e21d235eb24bcce8411c8de4e4ca
LuizQuirino <luizfpq@gmail.com>
2020-05-08 00:49:33 -0300

    fix semicolon on declaration, in other lang we miss it, in python we add an unecessary

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ for file in glob.glob("table/*.json"):
     with open(file, 'r') as f:
         table_dict = json.load(f)
         tableName = os.path.splitext(os.path.basename(file))[0]
-        serial = 0;
+        serial = 0
         for i in range(table_dict[0]['RecordsToGenerate']):
             valueList = ''
             DataList = list(table_dict[0]['DataType'].split(","))
