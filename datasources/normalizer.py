fb7b91f74b957b683cbbfe3172b51aa4f321c12e
Luiz Quirino <luizfpq@gmail.com>
2024-06-29 12:17:19 -0300

    fix normalizer

--- datasources/normalizer.py
+++ datasources/normalizer.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-__author__ = "Luiz F. P. Quirino"
-__copyright__ = "Copyleft 2020, Planet Earth"
-__credits__ = ["LuizQuirino"]
-__license__ = "GPL v3"
-__version__ = "2.0.1"
-__maintainer__ = "LuizQuirino"
-__email__ = "luizfpq@gmail.com"
-__status__ = "Dev"
 import os
 
-def normalize_files(folder_path, action='capitalize'):
+def process_files_in_folder(folder_path, action):
     if not os.path.exists(folder_path):
         print(f"A pasta '{folder_path}' não existe.")
         return
@@ -27,9 +16,8 @@ def normalize_files(folder_path, action='capitalize'):
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
-                if action == 'capitalize':
-                    modified_content = ' '.join(word.capitalize() for word in content.split())
-                elif action == 'uppercase':
+
+                if action == 'uppercase':
                     modified_content = content.upper()
                 elif action == 'lowercase':
                     modified_content = content.lower()
@@ -45,5 +33,5 @@ def normalize_files(folder_path, action='capitalize'):
         except IOError:
             print(f"Erro ao processar o arquivo '{file_name}'.")
 
-folder_path = '.'  # Diretório atual (onde este script está localizado)
-normalize_files(folder_path, action='capitalize')
+folder_path = '.'  # Diretório atual
+process_files_in_folder(folder_path, 'uppercase')
